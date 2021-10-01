322aa2dc86bb55d1976de895363456cffbe3297d
tacharya2 <akash_sharma430@yahoo.com>
2021-10-01 14:42:46 -0400

    test commit

--- src/main/java/com/example/flights/FlightsApplication.java
+++ src/main/java/com/example/flights/FlightsApplication.java
@@ -3,6 +3,7 @@ package com.example.flights;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Just added this comment
 @SpringBootApplication
 public class FlightsApplication {
 
